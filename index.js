429a29de61a12ff82fdf5b64e0a32aecf578ae92
fabio-coutinho-dos-santos <fabio.santcou@gmail.com>
2024-06-25 14:52:37 -0300

    feature: update function code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 export const handler = async (event) => {
-  console.log('Executing ...');
+  console.log('Executing ... updated');
+  console.log(event)
   const response = {
     statusCode: 200,
     body: JSON.stringify('Hello from Lambda!'),
